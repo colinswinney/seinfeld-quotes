7c52331f1f5419b3df18b1dcec7855768893783a
Colin Swinney <colinjswinney@gmail.com>
2020-07-22 15:23:35 -0500

    Updates

---
+++ precache-manifest.ccea8dbfe43983de950cc099b1e03ace.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "954a52aaaf326cce5455ccf47c11a57d",
+    "url": "/seinfeld-quotes/index.html"
+  },
+  {
+    "revision": "0e244fb5207d187df65d",
+    "url": "/seinfeld-quotes/static/css/main.71e6af6b.chunk.css"
+  },
+  {
+    "revision": "414d6b5635cb556c5062",
+    "url": "/seinfeld-quotes/static/js/2.5418b764.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/seinfeld-quotes/static/js/2.5418b764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e244fb5207d187df65d",
+    "url": "/seinfeld-quotes/static/js/main.5ab8a574.chunk.js"
+  },
+  {
+    "revision": "d32629d5306a9b7c046b",
+    "url": "/seinfeld-quotes/static/js/runtime-main.847f74aa.js"
+  },
+  {
+    "revision": "2991ebab717d44f86cc36a929abced26",
+    "url": "/seinfeld-quotes/static/media/FeniceStd-Bold.2991ebab.otf"
+  },
+  {
+    "revision": "aea793d101122c66ebe947326f9976cd",
+    "url": "/seinfeld-quotes/static/media/FeniceStd-Bold.aea793d1.woff"
+  },
+  {
+    "revision": "ba1355877cf8be7d956e7af898e038a4",
+    "url": "/seinfeld-quotes/static/media/FeniceStd-Bold.ba135587.woff2"
+  },
+  {
+    "revision": "20732d726f5b645f35f595a474764161",
+    "url": "/seinfeld-quotes/static/media/FeniceStd-Oblique.20732d72.woff2"
+  },
+  {
+    "revision": "59d9e45ec18c5fe5be08a21418296ff9",
+    "url": "/seinfeld-quotes/static/media/FeniceStd-Oblique.59d9e45e.woff"
+  },
+  {
+    "revision": "ceb779f9eacc844bd665b7bd4e581b80",
+    "url": "/seinfeld-quotes/static/media/FeniceStd-Oblique.ceb779f9.otf"
+  },
+  {
+    "revision": "6f3911fd8a92b9f936376c429119e4c6",
+    "url": "/seinfeld-quotes/static/media/FeniceStd-Regular.6f3911fd.otf"
+  },
+  {
+    "revision": "a42d5e8ee9cf48253cc41100f13d659e",
+    "url": "/seinfeld-quotes/static/media/FeniceStd-Regular.a42d5e8e.woff2"
+  },
+  {
+    "revision": "d3282b4be726ccbf1a9cc9ee02a6d485",
+    "url": "/seinfeld-quotes/static/media/FeniceStd-Regular.d3282b4b.woff"
+  },
+  {
+    "revision": "cd9bc5d8b5a703f22596d3b2d09e1546",
+    "url": "/seinfeld-quotes/static/media/cosmo-kramer-circle.cd9bc5d8.jpg"
+  },
+  {
+    "revision": "3fbcd0ef7d06402c6d34dceb4be113f3",
+    "url": "/seinfeld-quotes/static/media/dice.3fbcd0ef.png"
+  },
+  {
+    "revision": "8c1500b017f3847cb3bece093335af06",
+    "url": "/seinfeld-quotes/static/media/elaine-benes-circle.8c1500b0.jpg"
+  },
+  {
+    "revision": "5587743ec2af316179202a5e57f91673",
+    "url": "/seinfeld-quotes/static/media/frank-costanza-circle.5587743e.jpg"
+  },
+  {
+    "revision": "bf9b94463bc13047a47b743205d22f77",
+    "url": "/seinfeld-quotes/static/media/george-costanza-circle.bf9b9446.jpg"
+  },
+  {
+    "revision": "573bd40bbf01772805a7eaec27e1aa2a",
+    "url": "/seinfeld-quotes/static/media/jerry-seinfeld-circle.573bd40b.jpg"
+  }
+]);
